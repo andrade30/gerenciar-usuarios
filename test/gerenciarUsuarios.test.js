dbe0b1fb3ac475b87058cf18e4913c3adfa6cdf4
Alessandro Andrade <andrade.sp27@gmail.com>
2025-09-07 16:24:20 -0400

    Adicionando funções e testes

---
+++ test/gerenciarUsuarios.test.js
@@ -0,0 +1,14 @@
+const { retornarUsuarios } = require("../src/gerenciarUsuarios")
+const { adicionarNovoUsuario } = require("../src/gerenciarUsuarios")
+const { expect } = require('chai')
+
+describe('Testar as funções de Gestão de Usuários', function () {
+    it('Validar que posso adicionar um novo nome de usuário na lista', function () {
+        // 1. Adicionar um novo nome na lista de usuários
+        adicionarNovoUsuario('Alessandro');
+        // 2. Retornar a lista de usuários na caixa lista de usuários
+        const listaDeUsuarios = retornarUsuarios();
+        // 3. Comparar se o novo nome está no fim da lista de usuários
+        expect(listaDeUsuarios.at(-1)).to.equal('Alessandro');
+    });
+});
